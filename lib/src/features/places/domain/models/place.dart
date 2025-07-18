2ab1a57d8326f18e4b448a6875a731b4a5c616eb
DimitriSioutis <d.sioutis97@gmail.com>
2025-07-18 13:10:45 +0300

    resolve conflicts from merge with dev

--- lib/src/features/places/domain/models/place.dart
+++ lib/src/features/places/domain/models/place.dart
@@ -1,11 +1,6 @@
-<<<<<<<< HEAD:lib/src/features/places/data/models/place_model.dart
-import 'package:cloud_firestore/cloud_firestore.dart';
-import '../../domain/models/place.dart';
-========
 import 'package:equatable/equatable.dart';
->>>>>>>> dev:lib/src/features/places/domain/models/place.dart
 
-class PlaceModel {
+class Place extends Equatable {
   final String id;
   final String name;
   final String location;
@@ -15,7 +10,7 @@ class PlaceModel {
   final int price;
   final List<String> images;
 
-  const PlaceModel({
+  const Place({
     required this.id,
     required this.name,
     required this.location,
@@ -26,42 +21,6 @@ class PlaceModel {
     required this.images,
   });
 
-<<<<<<<< HEAD:lib/src/features/places/data/models/place_model.dart
-  factory PlaceModel.fromSnapshot(DocumentSnapshot doc) {
-    final data = doc.data() as Map<String, dynamic>;
-
-    List<String> images = [];
-    if (data['images'] != null) {
-      for (String image in data['images']) {
-        images.add(image);
-      }
-    }
-
-    return PlaceModel(
-      id: doc.id,
-      name: data['name'] ?? '',
-      location: data['location'] ?? '',
-      rating: (data['rating'] as num?)?.toDouble() ?? 0.0,
-      imageUrl: data['imageUrl'] ?? '',
-      description: data['description'] ?? '',
-      price: (data['price'] as num?)?.toInt() ?? 0,
-      images: images,
-    );
-  }
-
-  Place toDomain() {
-    return Place(
-      id: id,
-      name: name,
-      location: location,
-      rating: rating,
-      imageUrl: imageUrl,
-      description: description,
-      price: price,
-      images: images,
-    );
-  }
-========
   Map<String, dynamic> toJson() {
     return {
       'id': id,
@@ -77,5 +36,4 @@ class PlaceModel {
 
   @override
   List<Object> get props => [id, name, location, rating, imageUrl, description, price, images];
->>>>>>>> dev:lib/src/features/places/domain/models/place.dart
 }
